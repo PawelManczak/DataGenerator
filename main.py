f399ae66d850980b2f473cee95eadcbec9754631
PawelManczakInnse <pawel14522@gmail.com>
2023-11-07 13:01:37 +0100

    refund status

--- main.py
+++ main.py
@@ -54,20 +54,19 @@ DELAY_REASONS = ["whether", "crush", "other"]
 
 
 def flight_delay(flight_id: int, file):
-    if random.random() > 0.9:  # 10% chance
-        global flight_delay_id
-        file.write(str(flight_delay_id))
-        file.write("|")
-        file.write(str(flight_id))
-        file.write("|")
-        # delay duration
-        file.write(str(random.randint(0, 1800)))
-        file.write("|")
-        # delay reason
-        file.write(str(DELAY_REASONS[random.randint(0, len(DELAY_REASONS) - 1)]))
-        file.write('\n')
+    global flight_delay_id
+    file.write(str(flight_delay_id))
+    file.write("|")
+    file.write(str(flight_id))
+    file.write("|")
+    # delay duration
+    file.write(str(random.randint(0, 1800)))
+    file.write("|")
+    # delay reason
+    file.write(str(DELAY_REASONS[random.randint(0, len(DELAY_REASONS) - 1)]))
+    file.write('\n')
 
-        flight_delay_id += 1
+    flight_delay_id += 1
 
 
 reservation_id = 0
@@ -75,9 +74,16 @@ passenger_id = 0
 PAYMENT_METHODS = ["card", "cash"]
 
 
-def reservation(file, flight_id, ticket_class, price_of_economy, price_of_business, date, passenger_file):
+def reservation(file, flight_id, ticket_class, price_of_economy, price_of_business, date, passenger_file, is_delayed,
+                refund_file):
     global reservation_id
     global passenger_id
+
+    if (is_delayed and random.random() > 0.5) or random.random() > 0.8:
+        price = price_of_business if ticket_class == "business" else price_of_economy
+
+        ticket_refund(refund_file, reservation_id, date, price)
+
     file.write(str(reservation_id))
     file.write("|")
     file.write(str(flight_id))
@@ -117,6 +123,30 @@ def generate_passenger(passenger_id):
     return f"{passenger_id}|{first_name}|{last_name}|{date_of_birth}|{nationality}\n"
 
 
+refund_id = 0
+REFUND_REASONS = ["delay", "other"]
+REFUND_STATUS = ["accepted", "refused", "waiting"]
+
+
+def ticket_refund(refund_file, reservation_id, date, price):
+    global refund_id
+    refund_file.write(str(refund_id))
+    refund_file.write("|")
+    refund_file.write(str(reservation_id))
+    refund_file.write("|")
+
+    refund_date = date + timedelta(hours=random.randint(1, 12), minutes=random.randint(0, 59),
+                                   days=random.randint(0, 365))
+    refund_file.write(str(refund_date))
+    refund_file.write("|")
+    refund_file.write(str(price))
+    refund_file.write("|")
+    refund_file.write(random.choice(REFUND_REASONS))
+    refund_file.write("|")
+    refund_file.write(random.choice(REFUND_STATUS))
+    refund_id += 1
+
+
 if __name__ == '__main__':
     generate_airports()
     generate_airlines()
@@ -126,79 +156,85 @@ if __name__ == '__main__':
         with open('flight_delay.txt', 'w') as delay_file:
             with open('reservation.txt', 'w') as reservation_file:
                 with open('passenger.txt', 'w') as passenger_file:
-                    for id in range(N_OF_FLIGHTS):
-                        # id
-                        f.write(str(id))
+                    with open('ticket_refund.txt', 'w') as refund_file:
+                        for id in range(N_OF_FLIGHTS):
+                            is_delayed = False
+                            # ticket refund
+                            if random.random() > 0.9:  # 10% chance
+                                flight_delay(id, delay_file)
+                                is_delayed = True
 
-                        departure_date, arrival_date = generate_flight_dates()
-                        # departure date
-                        f.write(str(departure_date))
-                        f.write("|")
-                        # arrival date
-                        f.write(str(arrival_date))
-                        f.write("|")
-                        # airport code
-                        f.write(airport_codes[random.randint(0, N_OF_AIRPORTS - 1)])
-                        f.write("|")
+                            # id
+                            f.write(str(id))
+                            f.write("|")
+                            departure_date, arrival_date = generate_flight_dates()
+                            # departure date
+                            f.write(str(departure_date))
+                            f.write("|")
+                            # arrival date
+                            f.write(str(arrival_date))
+                            f.write("|")
+                            # airport code
+                            f.write(airport_codes[random.randint(0, N_OF_AIRPORTS - 1)])
+                            f.write("|")
 
-                        seat_data = generate_seat_counts()
-                        # NumberOfEconomySeats
-                        f.write(str(seat_data["economy_class_seats"]))
-                        f.write("|")
-                        # NumberOfBuissnessSeats
-                        f.write(str(seat_data["business_class_seats"]))
-                        f.write("|")
-                        # NumberOfEconomyPassengers
-                        f.write(str(seat_data["economy_class_occupied"]))
-                        f.write("|")
-                        # NumberOfBuisnessPassengers
-                        f.write(str(seat_data["business_class_occupied"]))
-                        f.write("|")
-                        # airline id
-                        f.write(str(random.randint(0, N_OF_AIRLINES - 1)))
-                        f.write("|")
+                            seat_data = generate_seat_counts()
+                            # NumberOfEconomySeats
+                            f.write(str(seat_data["economy_class_seats"]))
+                            f.write("|")
+                            # NumberOfBuissnessSeats
+                            f.write(str(seat_data["business_class_seats"]))
+                            f.write("|")
+                            # NumberOfEconomyPassengers
+                            f.write(str(seat_data["economy_class_occupied"]))
+                            f.write("|")
+                            # NumberOfBuisnessPassengers
+                            f.write(str(seat_data["business_class_occupied"]))
+                            f.write("|")
+                            # airline id
+                            f.write(str(random.randint(0, N_OF_AIRLINES - 1)))
+                            f.write("|")
 
-                        economy_price = random.randint(0, 500) + 250
-                        business_price = economy_price * 1.5
-                        # business price
-                        f.write(str(business_price))
-                        f.write("|")
-                        # economy price
-                        f.write(str(economy_price))
-                        f.write("|")
+                            economy_price = random.randint(0, 500) + 250
+                            business_price = economy_price * 1.5
+                            # business price
+                            f.write(str(business_price))
+                            f.write("|")
+                            # economy price
+                            f.write(str(economy_price))
+                            f.write("|")
+                            f.write('\n')
+                            # other files
 
-                        # other files
+                            for _ in range(seat_data["economy_class_occupied"]):
+                                reservation(reservation_file, id, "economy", economy_price, business_price,
+                                            departure_date,
+                                            passenger_file, is_delayed, refund_file)
+                            for _ in range(seat_data["business_class_occupied"]):
+                                reservation(reservation_file, id, "business", economy_price, business_price,
+                                            departure_date,
+                                            passenger_file, is_delayed,
+                                refund_file)
 
-                        for _ in range(seat_data["economy_class_occupied"]):
-                            reservation(reservation_file, id, "economy", economy_price, business_price, departure_date,
-                                        passenger_file)
-                        for _ in range(seat_data["business_class_occupied"]):
-                            reservation(reservation_file, id, "business", economy_price, business_price, departure_date,
-                                        passenger_file)
-
-                        flight_delay(id, delay_file)
-
-                        f.write('\n')
-
-"""
-najpierw sheet 2
-generujemy 20 lotnisk i mamy je w tabeli LOTNISKA
-później sheet 1
-Generujemy linie lotnicze i mamy je w tabelki
-
-Generujemy X Flight i itemrujemy po x
-Dla każdego x:
-    Losujemy daty
-    Airport code - tutaj losujemy z sheet2 - lotniska
-    Numbery losujemy od 60-120
-        Dla liczby pasażerów tworzymy tyle rezerwacji
-            I od razu random passenger
-         
-    Airline id bierzemy z sheet 1
-    ceny losujemy
-    
-    Flight delay ma 10% na utworzenie
-        dane random, reason z kilku opcji
-    Ticket refun ma 5% szns na utworzenie
-    
-"""
+                """
+                najpierw sheet 2
+                generujemy 20 lotnisk i mamy je w tabeli LOTNISKA
+                później sheet 1
+                Generujemy linie lotnicze i mamy je w tabelki
+                
+                Generujemy X Flight i itemrujemy po x
+                Dla każdego x:
+                    Losujemy daty
+                    Airport code - tutaj losujemy z sheet2 - lotniska
+                    Numbery losujemy od 60-120
+                        Dla liczby pasażerów tworzymy tyle rezerwacji
+                            I od razu random passenger
+                         
+                    Airline id bierzemy z sheet 1
+                    ceny losujemy
+                    
+                    Flight delay ma 10% na utworzenie
+                        dane random, reason z kilku opcji
+                    Ticket refun ma 5% szns na utworzenie
+                    
+                """
